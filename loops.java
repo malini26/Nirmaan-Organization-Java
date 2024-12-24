9f629bc01e2562fb477c5474a2fc0c7fc43b01fd
malini26 <126463485+malini26@users.noreply.github.com>
2024-12-24 15:44:41 +0530

    day4

---
+++ loops.java
@@ -0,0 +1,47 @@
+package day4;
+
+import java.util.Scanner;
+
+public class loops {
+	public static void main(String [] args) {
+		
+		for (int i = 1 ; i <= 4 ; i++) {
+			
+		
+		for(int j = 1 ; j <=i ; j++ ) {
+		     System.out.print("* ");
+		}
+		System.out.println();
+		}
+	}
+}
+		
+	   
+	
+
+
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter your table");
+//		int n = sc.nextInt();
+//					
+//		for( int i = 2 ;i<=10;i++){
+// 	    System.out.println("2x"+i+"="+i*2);
+//             }
+//	for	 ( int i = 1 ; i<= 100;i++) {
+//		if( i%5==0) {
+//			System.out.println("malini");
+//			
+//		}
+//		else {
+//			System.out.println(i);
+//		}
+//}
+	
+	 
+
+
+//for( int i = 10;i>=0;i--){
+//System.out.println(i);
+//	}
+//
