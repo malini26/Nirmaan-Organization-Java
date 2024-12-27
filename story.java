66d3679021491ce89354e3a9c72d6917f08a7af9
malini26 <126463485+malini26@users.noreply.github.com>
2024-12-27 16:05:59 +0530

    day5

---
+++ story.java
@@ -0,0 +1,34 @@
+package day5;
+
+public class story {
+	public static void main(String [] args) {
+	String name1 ="malini";
+	String adjective1 = " happy ";
+	String adjective2 = " good " ;
+	String noun1 = "students" ;
+	String noun2 ="chocolate ";
+	String verb1 ="dance" ;
+	String noun3 = "song" ;
+	String noun4 = "dogs" ;
+	String adjective3= "confused";
+	
+	String name2 = "ravi";
+	
+	String place1 = "home" ;
+
+	String noun5 = "icecream" ;
+
+	int number = 2024 ;
+	String noun6 = "cats";
+	
+	
+	
+	
+	
+	String child = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year " +number+ ", in a world where "+noun6+"s ruled the world.";
+	
+  System.out.println(child);
+	
+			
+}
+}
