1394763ee186dc0c6837da8930bac111784502cf
malini26 <126463485+malini26@users.noreply.github.com>
2024-12-30 17:16:21 +0530

    day6

---
+++ reverse.java
@@ -0,0 +1,19 @@
+package day6;
+
+public class reverse {
+
+	public static void main(String[] args) {
+//		String name ="malini";
+//		
+//	   String rs = "" ;
+//	   
+//	   for(int i = 5 ; i>=0 ; i --) {
+//		   rs += name.charAt(i);
+//		   
+//		   
+//	   }
+//	   System.out.println(rs.toUpperCase());
+		
+//			   
+	}
+} 
